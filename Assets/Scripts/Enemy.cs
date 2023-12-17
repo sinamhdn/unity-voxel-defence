b1201a793570bcfb0b59a0e0f0efc7cdb8650037
SinaMHDN <sina.mh.id@gmail.com>
2023-12-17 10:58:20 +0330

    Add particle effects to enemy prefab

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] ParticleSystem hitParticle;
+    [SerializeField] ParticleSystem deathParticle;
     [SerializeField] int hitPoints = 300;
     [SerializeField] int particlesHit = 0;
 
@@ -28,10 +30,13 @@ public class Enemy : MonoBehaviour
     void getHit()
     {
         hitPoints = hitPoints - 1;
+        hitParticle.Play();
     }
 
     void getKilled()
     {
+        var deathVFX = Instantiate(deathParticle, transform.position, Quaternion.identity);
+        deathVFX.Play();
         Destroy(gameObject);
     }
 }
